ea37be55c81b54095d044620f4fd1f9ae9967763
jason-sung <48958837+jason-sung@users.noreply.github.com>
2019-04-12 15:52:54 +0000

    Update send.php

--- send.php
+++ send.php
@@ -4,10 +4,12 @@
 require 'includes/dbh.inc.php';
 if(isset($_POST["textinfo"]))
 {
- 
+ //CV page message request with purpose and message 
  $purpose = mysqli_real_escape_string($con, $_POST["purpose"]);
  $textinfo = mysqli_real_escape_string($con, $_POST["textinfo"]);
  $id = mysqli_escape_string($conn,$_GET['id']);
+ 
+ //Insertion to comments table
  $query = "
  INSERT INTO comments(id,comment_subject, comment_text)
  
