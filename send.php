f56f9ac98a15fce592baf18e717f9578ff2579e1
skgahlyan <48961206+skgahlyan@users.noreply.github.com>
2019-04-12 15:59:54 +0100

    Add files via upload
    send message from hire me button
    Open a new page with purpose and text
    Not sure if it's working completely  but sometime it doesn't get user id from users table and update in comments table.

---
+++ send.php
@@ -0,0 +1,20 @@
+<!--Code Help From https://www.cloudways.com/blog/real-time-php-notification-system/-->
+<?php
+//insert.php
+require 'includes/dbh.inc.php';
+if(isset($_POST["textinfo"]))
+{
+ 
+ $purpose = mysqli_real_escape_string($con, $_POST["purpose"]);
+ $textinfo = mysqli_real_escape_string($con, $_POST["textinfo"]);
+ $id = mysqli_escape_string($conn,$_GET['id']);
+ $query = "
+ INSERT INTO comments(id,comment_subject, comment_text)
+ 
+ VALUES ('$purpose', '$textinfo')
+ ";
+ $query2="INSERT INTO comments(id)
+ SELECT id FROM [users] WHERE id NOT IN (SELECT id FROM comments)";
+ mysqli_query($con, $query,$query2);
+}
+?>
